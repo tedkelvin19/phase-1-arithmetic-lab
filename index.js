e1d909647501311181dbfddf981bb6d2d9875d0b
TedKelvin <tedkelvin19@gmail.com>
2023-09-19 00:49:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1 = 1;
+const num2 = 62;
+const num3 = 22;
+const num4 = 6;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 11) + 1;
+let mod = num3 % num4;
+let max = Math.max(1, 20);
